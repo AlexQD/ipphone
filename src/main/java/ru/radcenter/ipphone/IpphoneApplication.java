15775027a9995f1e74a50490ca3b9a741d379d4c
AlexQD <Alexqd@ya.ru>
2020-11-10 21:04:52 +0400

    init

---
+++ src/main/java/ru/radcenter/ipphone/IpphoneApplication.java
@@ -0,0 +1,13 @@
+package ru.radcenter.ipphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpphoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpphoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/radcenter/ipphone/IpphoneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.radcenter.ipphone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpphoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
