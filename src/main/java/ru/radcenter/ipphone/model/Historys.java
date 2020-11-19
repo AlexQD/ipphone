107857e6be00ef7c17fa78d6add2d12dc2d64a2a
Alexqd <alexqd@ya.ru>
2020-11-19 10:24:27 +0400

    Вынесена  логика из модели в сервис

--- src/main/java/ru/radcenter/ipphone/model/Historys.java
+++ src/main/java/ru/radcenter/ipphone/model/Historys.java
@@ -14,9 +14,6 @@ import java.util.Set;
 public class Historys {
     private ArrayList<History> historyArray;
 
-    @Autowired
-    private HistoryService historyService;
-
     public Historys(){
         historyArray = new ArrayList<>();
     }
@@ -33,47 +30,7 @@ public class Historys {
         historyArray.add(history);
     }
 
-
-    public void print(){
-        for (int i = 0; i < historyArray.size(); ++i) {
-            System.out.println(historyArray.get(i).getUidAtc());
-
-        }
-    }
-
-    public Historys missingCall(History history){
-        Historys missingCallsClient = new Historys();
-        for (int i = 0; i < historyArray.size(); ++i) {
-            if (historyArray.get(i).getClient().equals(history.getClient())){
-                missingCallsClient.add(historyArray.get(i));
-            }
-        }
-        return missingCallsClient;
-    }
-
-    public void delDublicate(){
-        HashMap<String, History> hashMap = new HashMap<>();
-        for (int i = 0; i < historyArray.size(); ++i) {
-            hashMap.put(historyArray.get(i).getClient(),historyArray.get(i));
-        }
+    public void clear(){
         historyArray.clear();
-        historyArray = new ArrayList<>(hashMap.values());
     }
-
-
-    public void saveBd(){
-        for (int i = 0; i < historyArray.size(); ++i) {
-            historyService.save(historyArray.get(i));
-        }
-    }
-
-//    public static Historys missing_call(Historys historys){
-//        for (int i = 0; i < historys.getHistory_array().size(); ++i) {
-//            if (historys.getHistory_array().get(i).getType().equals("missed")){
-//
-//            }
-//            //System.out.println(history_array.get(i).getUid_atc());
-//
-//        }
-//    }
 }
--- src/main/java/ru/radcenter/ipphone/services/HistorysServiceImpl.java
+++ src/main/java/ru/radcenter/ipphone/services/HistorysServiceImpl.java
@@ -3,6 +3,11 @@ package ru.radcenter.ipphone.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.radcenter.ipphone.dto.HistorysDto;
+import ru.radcenter.ipphone.model.History;
+import ru.radcenter.ipphone.model.Historys;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Service
 public class HistorysServiceImpl implements  HistorysService {
@@ -18,4 +23,32 @@ public class HistorysServiceImpl implements  HistorysService {
             historyService.save(historysDto.getHistoryArray().get(i));
         }
     }
+    public void print(Historys historys){
+        for (int i = 0; i < historys.getHistoryArray().size(); ++i) {
+            System.out.println(historys.getHistoryArray().get(i).getUidAtc());
+
+        }
+    }
+
+    public void delDublicate(Historys historys){
+        HashMap<String, History> hashMap = new HashMap<>();
+        for (int i = 0; i < historys.getHistoryArray().size(); ++i) {
+            hashMap.put(historys.getHistoryArray().get(i).getClient(),historys.getHistoryArray().get(i));
+        }
+        historys.clear();
+        historys.setHistoryArray( new ArrayList<>(hashMap.values()));
+    }
+
+    public Historys missingCall(Historys historys, History history){
+        Historys missingCallsClient = new Historys();
+        for (int i = 0; i < historys.getHistoryArray().size(); ++i) {
+            if (historys.getHistoryArray().get(i).getClient().equals(history.getClient())){
+                missingCallsClient.add(historys.getHistoryArray().get(i));
+            }
+        }
+        return missingCallsClient;
+    }
+
+
+
 }
