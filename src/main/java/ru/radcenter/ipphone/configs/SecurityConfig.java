2930b147f37b4e739e3159ba182f6763d03d021a
Alexqd <alexqd@ya.ru>
2020-12-05 10:52:39 +0400

    убраны комментарии

--- src/main/java/ru/radcenter/ipphone/configs/SecurityConfig.java
+++ src/main/java/ru/radcenter/ipphone/configs/SecurityConfig.java
@@ -32,55 +32,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                //.antMatchers("/authenticated/**").authenticated()
                 .antMatchers("/gethistory/**").hasRole("ADMIN")
-                //.antMatchers("/read_profile/**").hasAuthority("READ_PROFILE")
                 .and()
                 .formLogin()
                 .and()
                 .logout().logoutSuccessUrl("/");
     }
 
-//    In-Memory
-//    @Bean
-//    public UserDetailsService users() {
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password("{bcrypt}$2y$12$0./a1TvfPwxj.OTlQLF1HONCpHXHtrcrn7CR5sPE7UHSWLbkPjxZG")
-//                .roles("USER")
-//                .build();
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password("{bcrypt}$2y$12$0./a1TvfPwxj.OTlQLF1HONCpHXHtrcrn7CR5sPE7UHSWLbkPjxZG")
-//                .roles("ADMIN", "USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
-    //    jdbcAuthentication
-//    @Bean
-//    public JdbcUserDetailsManager users(DataSource dataSource) {
-////        UserDetails user = User.builder()
-////                .username("user")
-////                .password("{bcrypt}$2y$12$0./a1TvfPwxj.OTlQLF1HONCpHXHtrcrn7CR5sPE7UHSWLbkPjxZG")
-////                .roles("USER")
-////                .build();
-////        UserDetails admin = User.builder()
-////                .username("admin")
-////                .password("{bcrypt}$2y$12$0./a1TvfPwxj.OTlQLF1HONCpHXHtrcrn7CR5sPE7UHSWLbkPjxZG")
-////                .roles("ADMIN", "USER")
-////                .build();
-//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-////        if (jdbcUserDetailsManager.userExists(user.getUsername())) {
-////            jdbcUserDetailsManager.deleteUser(user.getUsername());
-////        }
-////        if (jdbcUserDetailsManager.userExists(admin.getUsername())) {
-////            jdbcUserDetailsManager.deleteUser(admin.getUsername());
-////        }
-////        jdbcUserDetailsManager.createUser(user);
-////        jdbcUserDetailsManager.createUser(admin);
-//        return jdbcUserDetailsManager;
-//    }
 
     @Bean
     public JdbcUserDetailsManager users(DataSource dataSource) {
